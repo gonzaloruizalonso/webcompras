02dd98ae20d659e06a65ee1a0ca120a2a4942237
gonzalo.ruiz <56383879+gonzaloruizalonso@users.noreply.github.com>
2020-01-17 13:19:03 +0100

    Add files via upload

---
+++ files/conexion.php
@@ -0,0 +1,12 @@
+<?php
+/* Conexión BD */
+define('DB_SERVER', '10.130.7.128');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'rootroot');
+define('DB_DATABASE', 'COMPRASWEB');
+$conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   if (!$conn) {
+		die("Error conexión: " . mysqli_connect_error());
+	}
+?>
